6795f2993b4207f8a7aacb33897bf07353304684
Bruno Gustavo Busarelloq <150341182+brunobusarello@users.noreply.github.com>
2024-05-26 19:19:45 -0300

    configuração dos inputs conforme clicado

--- pages/pixTransferPage/script.js
+++ pages/pixTransferPage/script.js
@@ -1,4 +1,4 @@
-function cpf(){
+/*function cpf(){
     document.getElementById("cpf").style.opacity="100%";
     document.getElementById("celular").style.opacity="0%";
     document.getElementById("email").style.opacity="0%";
@@ -24,4 +24,30 @@ function randomkey(){
     document.getElementById("celular").style.opacity="0%";
     document.getElementById("email").style.opacity="0%";
     document.getElementById("randomkey").style.opacity="100%";
+}*/
+function typeInput(caller){
+    if(caller.id === 'cnpj'){
+        $("#cpf").attr("style", "display: block");
+        $("#celular").attr("style", "display: none");
+        $("#email").attr("style", "display: none");
+        $("#randomkey").attr("style", "display: none");
+    }
+    else if(caller.id === 'cel'){
+        $("#cpf").attr("style", "display: none");
+        $("#celular").attr("style", "display: block");
+        $("#email").attr("style", "display: none");
+        $("#randomkey").attr("style", "display: none");
+    }
+    else if(caller.id === 'em'){
+        $("#cpf").attr("style", "display: none");
+        $("#celular").attr("style", "display: none");
+        $("#email").attr("style", "display: block");
+        $("#randomkey").attr("style", "display: none");
+    }
+    else if(caller.id === 'random'){
+        $("#cpf").attr("style", "display: none");
+        $("#celular").attr("style", "display: none");
+        $("#email").attr("style", "display: none");
+        $("#randomkey").attr("style", "display: block");
+    }
 }
