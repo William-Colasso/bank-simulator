136791ebe5d39a3477b0f3dc40227112d10b3ada
Lucas Gabriel <152902075+luquinhaixx@users.noreply.github.com>
2024-06-05 16:43:15 -0300

    Aula 05/06 - Gerson

---
+++ pages/copyPage/script.js
@@ -0,0 +1,14 @@
+const copyArea = document.querySelector(".copy-area");
+const loading = document.querySelector(".loading");
+const text = document.querySelector(".copy-text");
+
+function click(){
+    copyArea.addEventListener("click", (e) => {
+        if(e.)
+        loading.style.width++
+        loading.style.height++
+    });
+    click();
+}
+
+click()
