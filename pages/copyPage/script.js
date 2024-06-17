055b1f2f8dab842563b9e10df778fe3d4ce3cb18
Lucas Gabriel <152902075+luquinhaixx@users.noreply.github.com>
2024-06-17 15:55:58 -0300

    Summary (required)

--- pages/copyPage/script.js
+++ pages/copyPage/script.js
@@ -1,8 +1,6 @@
 const copyArea = document.querySelector(".copy-area");
 const text = document.querySelector(".copy-text");
 const loading = document.querySelector(".loading");
-var clicking = true;
-var ola = 2 + 2;
 
 async function paste() {
     const copiedText = await navigator.clipboard.readText()
@@ -10,14 +8,7 @@ async function paste() {
 } 
 
 copyArea.addEventListener("mousedown", (event) => {
-    clicking = true
-    console.log("Mousedown");
-    while (clicking) {
-        copyArea.addEventListener("mouseup", (event) => {
-            clicking = false
-            console.log("Mouseup");
-        });
-    }
+    paste();
 });
 
 
