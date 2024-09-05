882811264ba46dcbf09f8dd223a3328375a7ce1d
Júlio Henrique Busarello <165197218+JulioBusarello@users.noreply.github.com>
2024-09-04 21:35:48 -0300

    Concerto cores icones e seleção do tipo

--- pages/chaves/cadastrar/script.js
+++ pages/chaves/cadastrar/script.js
@@ -1,26 +1,24 @@
-function typeInput(caller){
-    if(caller.id === 'cnpj'){
-        $("#cpf").attr("style", "display: flex");
-        $("#celular").attr("style", "display: none");
-        $("#email").attr("style", "display: none");
-        $("#randomkey").attr("style", "display: none");
-    }
-    else if(caller.id === 'cel'){
-        $("#cpf").attr("style", "display: none");
-        $("#celular").attr("style", "display: flex");
-        $("#email").attr("style", "display: none");
-        $("#randomkey").attr("style", "display: none");
-    }
-    else if(caller.id === 'em'){
-        $("#cpf").attr("style", "display: none");
-        $("#celular").attr("style", "display: none");
-        $("#email").attr("style", "display: flex");
-        $("#randomkey").attr("style", "display: none");
-    }
-    else if(caller.id === 'random'){
-        $("#cpf").attr("style", "display: none");
-        $("#celular").attr("style", "display: none");
-        $("#email").attr("style", "display: none");
-        $("#randomkey").attr("style", "display: flex");
-    }
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('main .button');
+    const confirmButton = document.querySelector('a.confirmar');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            // Remove a classe 'selected' de todos os botões
+            buttons.forEach(btn => btn.classList.remove('selected'));
+
+            // Adiciona a classe 'selected' ao botão clicado
+            button.classList.add('selected');
+
+            // Adiciona a classe 'active' ao botão confirmar
+            confirmButton.classList.add('active');
+        });
+    });
+
+    // Adiciona a lógica para o botão "CONFIRMAR" ser desativado se nenhum botão for selecionado
+    confirmButton.addEventListener('click', () => {
+        if (!document.querySelector('main .button.selected')) {
+            confirmButton.classList.remove('active');
+        }
+    });
+});
